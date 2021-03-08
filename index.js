41f6e92dcb18c6d7ff1ffdf572d39867d98ef145
403-html <tymoteusz.stepien@wp.pl>
2021-03-08 20:29:17 +0100

    add cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import bodyParser from "body-parser";
 import express from "express";
+import cors from "cors";
 
 import usersRouter from "./routers/users.js";
 
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use("/users", usersRouter);
 
 app.get("/", (req, res) => {
